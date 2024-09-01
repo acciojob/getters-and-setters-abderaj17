c6aaba60b98880627a597dde11c930a2445aff0e
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2024-09-01 20:01:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age){
+		this.name = name;
+		this.age=age;
+	}
+    set age1(ageValue){
+        console.log(this.age);
+    }
+    get name1(){
+      return this.name;
+    }
+}
 
-class Student extends Person {}
+class Student extends Person {
+    constructor(name, age, ){
+        super(name, age);
+    }
+    study = ()=>{
+        console.log(this.name + "is studying");
+    }
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+    constructor(name,age){
+        super(name, age);
+    }
+    teach=()=>{
+        console.log(this.name + "is teaching");
+    }
+}
+Person.name1="Adarsh";
+console.log(Person.name1)
 
 // Do not change the code below this line
 window.Person = Person;
